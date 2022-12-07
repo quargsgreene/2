60c2b4d47ce3b24e7aea708e589cd54c47df4ce8
Quargs Greene <quargsgreene@gmail.com>
2022-12-07 12:28:52 -0800

    changed tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe('', () => {
+describe('Meat Suit Scrambler browser tests', () => {
   it('visits the page', () => {
     cy.visit('https://quargsgreene.github.io/meat-suit-scrambler/dist/index.html');
   });
